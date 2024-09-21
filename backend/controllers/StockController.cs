cfd5a25d47a2fba7b5707319e092b224e55ff6a8
EnzoMarco01 <emarco@uade.edu.ar>
2024-09-21 17:30:24 -0300

    creacion de stock controller

---
+++ backend/controllers/StockController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _dBContext; 
+        public StockController(ApplicationDBContext dBContext)
+        {
+            _dBContext = dBContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _dBContext.Stocks.ToList();
+
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute]int id)
+        {
+            var stock = _dBContext.Stocks.Find(id);
+
+            if (stock == null) 
+            {
+                return NotFound();
+            }
+
+            return Ok(stock);
+        }
+
+
+    }
+}
